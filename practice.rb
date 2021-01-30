9f9d3e0bd37a4c50a7d44d7bbcc607f71517cbad
George Serna <sernageorge@gmail.com>
2021-01-30 04:49:57 +0000

    Automatically backed up by Learn

--- practice.rb
+++ practice.rb
@@ -1,7 +1,5 @@
-def current_age_for_birth_year(birth_year)
+
 current_year = time.now.year
 birth_year = 1991
-  puts = "I am #{birth_year}"
-end
 
-current_age_for_birth_year(1991)
+puts "the year now is 'current_year'"
