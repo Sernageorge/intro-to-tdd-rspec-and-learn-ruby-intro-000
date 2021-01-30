84ffc8058743cdc2c619aa612738190fa7a12195
George Serna <sernageorge@gmail.com>
2021-01-30 04:46:56 +0000

    Automatically backed up by Learn

---
+++ practice.rb
@@ -0,0 +1,8 @@
+def current_age_for_birth_year(birth_year)
+current_year = time.now.year
+birth_year = 1991
+  puts = "I am #{birth_year}"
+end
+
+answer = current_year - birth_year
+age_of_person = current_age_for_birth_year(birth_year)
